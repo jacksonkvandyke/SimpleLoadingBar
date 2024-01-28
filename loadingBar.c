726f1170109b97ea04bbb23c90c6f4a87aab0463
jacksonkvandyke <102544560+jacksonkvandyke@users.noreply.github.com>
2024-01-27 18:30:12 -0600

    first commit

---
+++ loadingBar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main(){
+
+    int index = 0;
+    int percentDone = 0;
+    char loadingBar[51] = {};
+    memset(&loadingBar, '-', 50);
+    for(;;){
+        printf("\rLoading: %s %d%c", loadingBar, percentDone, '%');
+        loadingBar[index] = '>';
+        if ((index - 1) >= 0) loadingBar[index - 1] = '=';
+        index += 1;
+        percentDone = 100 - (2 * (50 - index));
+        sleep(1);
+    }
+
+}
